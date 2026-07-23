cf7934de13015dfd3ab6be8a327ed0431328b04a
Phax <sisnolan66@gmail.com>
2026-07-23 03:49:40 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,4 +1,3 @@
-require('dotenv').config();
 const { Client, GatewayIntentBits, Partials } = require('discord.js');
 const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMembers, GatewayIntentBits.GuildBans], partials: [Partials.User] });
 
@@ -23,7 +22,6 @@ client.on('interactionCreate', async interaction => {
             return;
         }
 
-        // Failed attemptes.
         try {
             const bans = await guild.bans.fetch();
             if (bans.size === 0) {
